972f73cc97811ab498f8339b5f6387bd5f888e3a
Tiedapang <2750428832@qq.com>
2020-11-13 09:59:57 +0800

    [thj] fininshed the pratise 3

--- src/practice3/index.js
+++ src/practice3/index.js
@@ -1,1 +1,13 @@
-export const getPoetry
+export const getPoetry = () => {
+    fetch("https://v1.jinrishici.com/all.json")
+        .then((response) => response.json())
+        .then((date) => [date.origin, date.author, date.content])
+        .catch((error) => console.info(error));
+}
+
+export const getPoetry1 = async () => {
+    await fetch("https://v1.jinrishici.com/all.json")
+        .then((response) => response.json())
+        .then((date) => [date.origin, date.author, date.content])
+        .catch((error) => console.info(error));
+}
